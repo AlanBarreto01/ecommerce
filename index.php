f9541c2e25c15f7c989e860e2e7326367d8558a3
Alan Barreto <almaceuber@gmail.com>
2022-10-01 17:45:37 -0300

    Site - Categorias

--- index.php
+++ index.php
@@ -332,6 +332,22 @@ $app->post("/admin/categories/:idcategory", function($idcategory)//colocou na ro
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory)
+	{
+
+		$category = new Category();
+
+		$category->get((int)$idcategory);
+
+		$page = new Page();
+
+		$page->setTpl("category",[
+			'category'=>$category->getValues(),
+			'products'=>[]
+		]);
+
+	});
+
 $app -> run();
 
  ?>
